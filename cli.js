7f5eb010803f4fb98554036cbc86d7427b250e37
willyb321 <wbwilliam7@gmail.com>
2016-09-18 15:02:45 +1000

    added regex to only allow numbers (probs)

--- cli.js
+++ cli.js
@@ -8,11 +8,16 @@ const schema = {
 	properties: {
 		dlSpeed: {
 			description: 'Download speed in megabytes/s',
-			required: true
+			required: true,
+			message: 'Please use a number.',
+			pattern: /[\d*\.?\d*]/
+
 		},
 		fileSize: {
 			description: 'File Size in megabytes',
-			required: true
+			required: true,
+			message: 'Please use a number.',
+			pattern: /[\d*\.?\d*]/
 		}
 	}
 };
@@ -25,11 +30,11 @@ function onErr(err) {
 }
 
 prompt.get(schema, (err, result) => {
-	const dlspeed = result.dlSpeed;
-	const filesize = result.fileSize;
 	if (err) {
 		return onErr(err);
 	}
+	const dlspeed = result.dlSpeed;
+	const filesize = result.fileSize;
 	const dlTime = filesize / dlspeed;
 	const dlTimePretty = moment.duration(dlTime, 'seconds');
 	console.log('Your approximate DL Time is: ' + dlTimePretty.humanize(false));
